b9c62e39609849fed21948f5df860b8f8fec09d1
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-02-16 20:45:01 -0300

    Refactored Header file component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+import { Container } from '@/styles/utils'
+
+export const StyledHeader = styled.header`
+  width: 100vw;
+  margin: 1.75rem 0 3.125rem 0;
+`;
+
+export const NavBar = styled.nav`
+  ${Container};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const MenuList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  gap: 2.5rem;
+  align-items: center;
+`;
+
+export const MenuItem = styled.li`
+  font-size: 16px;
+  font-weight: 700;
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+  a:hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,10 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>Imagineshop</title>
+    </Head>
+
     <Header />
     </>
   )
