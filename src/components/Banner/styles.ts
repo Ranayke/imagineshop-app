774f208545b5437a1282fc934cf58fe42bee7120
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-03-07 10:47:16 -0300

    feat(product): Finished product details page

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -4,5 +4,6 @@ import { Container } from "@/styles/utils";
 export const BannerContainer = styled.section`
   ${Container};
   border-top: 3px solid ${({theme}) => theme.colors.primary};
-  border-bottom: 3px solid ${({theme}) => theme.colors.primary}
+  border-bottom: 3px solid ${({theme}) => theme.colors.primary};
+  padding-top: 4px;
 `
--- src/pages/products/[productId].tsx
+++ src/pages/products/[productId].tsx
@@ -1,39 +1,90 @@
 import { GetServerSideProps } from "next";
-import { ProductContainer } from "./styles";
+import {
+  Button,
+  ImageContainer,
+  ProductContainer,
+  ProductDescription,
+  ProductDetail,
+  ProductName,
+  ProductPrice,
+  ProductSplitPrice,
+  Summary,
+  SummaryTitle,
+} from "./styles";
 
 import Banner from "@/components/Banner";
-import BannerImage from "../../../public/images/banner2.png"
+import BannerImage from "../../../public/images/banner2.png";
+import Image from "next/image";
+
+interface Product {
+  _id: string;
+  name: string;
+  image: string;
+  price: number;
+  formattedPrice: string;
+  splitPrice: string;
+  fileName: string;
+  description: string;
+  summary: string;
+}
+
+interface ProductsProps {
+  product: Product;
+}
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const productId = ctx.params?.productId;
   const api = "https://imagineshopapi.fly.dev";
   const result = await fetch(`${api}/products/${productId}`);
-  const data = await result.json();
+  const product: Product = await result.json();
 
-  data.image = `${api}/uploads/${data.fileName}`;
-  data.formattedPrice = new Intl.NumberFormat("pt-BR", {
+  product.image = `${api}/uploads/${product.fileName}`;
+  product.formattedPrice = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
-  }).format(data.price);
-  data.splitPrice = new Intl.NumberFormat("pt-BR", {
+  }).format(product.price);
+  product.splitPrice = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
-  }).format(data.price / 10);
+  }).format(product.price / 10);
   return {
     props: {
-      data,
+      product,
     },
   };
 };
 
-export default function ProductId({ data }: any) {
+export default function ProductId({ product }: ProductsProps) {
   return (
     <>
-    <ProductContainer>
+      <ProductContainer>
         <Banner image={BannerImage} width={1140} height={145} />
-      <h1>{data.name}</h1>
-    </ProductContainer>
+
+        <ProductDetail>
+          <ImageContainer>
+            <Image
+              alt="Product Image"
+              src={product.image}
+              width={200}
+              height={200}
+            ></Image>
+          </ImageContainer>
+
+          <div>
+            <ProductName>{product.name}</ProductName>
+            <ProductPrice>{product.formattedPrice}</ProductPrice>
+            <ProductSplitPrice>
+              10x de {product.splitPrice} sem juros
+            </ProductSplitPrice>
+            <Button>Adicionar ao carrinho</Button>
+            <ProductDescription>{product.description}</ProductDescription>
+          </div>
+        </ProductDetail>
+        <SummaryTitle>
+          <span>Inf</span>ormações do produto
+        </SummaryTitle>
+        <Summary>{product.summary}</Summary>
+      </ProductContainer>
     </>
   );
 }
-
