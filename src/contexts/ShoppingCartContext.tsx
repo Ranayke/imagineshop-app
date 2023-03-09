cb6d088f7f106f0a2df99064734f1cf820a1efbe
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-03-08 22:03:25 -0300

    feat(shoppingCart): Started shopping cart page

--- src/contexts/ShoppingCartContext.tsx
+++ src/contexts/ShoppingCartContext.tsx
@@ -4,29 +4,33 @@ import { IProduct } from "@/types";
 
 interface ShoppingCart {
   addProduct: (product: IProduct) => void;
+  getProducts: () => IProduct[];
 }
 
 export const ShoppingCartContext = createContext({} as ShoppingCart);
 
 export const ShoppingCartProvider = ({ children }: any) => {
-
-    const isBrowser = typeof window !== 'undefined';
-    const SESSION_STORAGE = 'products'
+  const isBrowser = typeof window !== "undefined";
+  const SESSION_STORAGE = "products";
 
   const addProduct = (product: IProduct) => {
-    const products = getProduct()
-    products.push(product)
-    if(isBrowser) {
-        sessionStorage.setItem(SESSION_STORAGE, JSON.stringify(products))
+    const products = getProducts();
+    products.push(product);
+    if (isBrowser) {
+      sessionStorage.setItem(SESSION_STORAGE, JSON.stringify(products));
     }
   };
 
-  const getProduct = (): IProduct[] => {
+  const getProducts = (): IProduct[] => {
+    if (isBrowser) {
+      const products = sessionStorage.getItem(SESSION_STORAGE);
+      return products ? JSON.parse(products) : [];
+    }
     return [];
   };
 
   return (
-    <ShoppingCartContext.Provider value={{ addProduct }}>
+    <ShoppingCartContext.Provider value={{ addProduct, getProducts }}>
       {children}
     </ShoppingCartContext.Provider>
   );
