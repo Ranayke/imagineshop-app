24cc370aa336d8ece8cea420c1e10e05d25621ae
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2023-03-09 22:58:20 -0300

    feat(shoppingCart): Started to construct shopping cart page

---
+++ src/pages/shopping-cart/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+
+export const Main = styled.main`
+
+`
+
+export const Title = styled.p`
+
+`
+
+export const SubTitle = styled.p`
+
+`
+
+export const ShoppingCartContainer = styled.div`
+
+`
+
+export const ShoppingCartProducts = styled.section`
+
+`
+
+export const Separator = styled.hr`
+
+`
+
+export const ButtonContainer = styled.button`
+
+`
+export const Product = styled.div`
+
+`
